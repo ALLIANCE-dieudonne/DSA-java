bb0a0efe8b758ce4bd46865bedc5e09719d10aaf
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-12-31 18:42:51 +0200

    linked list

---
+++ src/linkedList/Main.java
@@ -0,0 +1,11 @@
+package src.linkedList;
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+    list.insert(12);
+    list.insert(10);
+    list.insert(5);
+    list.show();
+  }
+}
---
+++ src/linkedList/Node.java
@@ -0,0 +1,6 @@
+package src.linkedList;
+
+public class Node {
+  int data;
+  Node next;
+}
