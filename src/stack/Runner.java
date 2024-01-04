8fde18bad3e6b25d376d81101f84e4507eb903fc
ALLIANCE-dieudonne <filalliance769@gmail.com>
2024-01-04 20:38:25 +0200

    done with dynamic stack

--- src/stack/Runner.java
+++ src/stack/Runner.java
@@ -4,13 +4,22 @@ package src.stack;
 public class Runner {
   public static void main(String[] args) {
     Stack nums = new Stack();
-    nums.push(12);
-    nums.show();
-    nums.push(10);
-    nums.show();
-    nums.pop();
-    nums.pop();
-    nums.pop();
-    nums.show();
+//    nums.push(12);
+//    nums.show();
+//    nums.push(10);
+//    nums.show();
+
+    DynamicStack Dstack = new DynamicStack();
+    Dstack.push(12);
+    Dstack.show();
+    Dstack.push(10);
+    Dstack.show();
+    Dstack.push(12);
+    Dstack.show();
+    Dstack.push(10);
+    Dstack.show(); Dstack.show();
+    Dstack.push(10);
+    Dstack.show();
+
   }
 }
